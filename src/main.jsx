3bd3249368ca91758767e068a00f4296a5fea6e0
Lakshanan-pro <lakshanan14@gmail.com>
2026-09-05 20:47:00 +0530

    Add React application entry point

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PlantRecommender from "./J6.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PlantRecommender />
+  </React.StrictMode>
+);
